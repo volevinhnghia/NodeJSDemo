c0c959b2a4e67f4c779e7161762fd489fd9db9d2
Vinh Nghia <vn29082000@gmail.com>
2023-06-14 13:41:33 +0700

    change to RES api

--- src/routes/book.js
+++ src/routes/book.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const bookControllers = require('../controllers/book');
 
-router.get("/get-all-books", bookControllers.getAllBooks)
+router.get("/books", bookControllers.getAllBooks)
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const usersControllers = require('../controllers/users');
 
 router.get('/users', usersControllers.getAllUsers);
-router.get('/users/:userID', usersControllers.getUserByID);
-router.get('/users/', usersControllers.getUserByNameAndAge);
-router.post('/users/', usersControllers.createUser);
-router.post('/users/', usersControllers.createManyUsers);
+router.get('/users/:userId', usersControllers.getUserByID);
+router.get('/users/user-by-name-and-age', usersControllers.getUserByNameAndAge);
+router.post('/users', usersControllers.createUser);
 
 module.exports = router;
